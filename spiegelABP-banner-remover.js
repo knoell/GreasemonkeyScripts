bdd906690c3e2761dad297f22109b5a00582cd55
knoell <knoell@users.noreply.github.com>
2018-03-17 17:25:21 +0100

    Update spiegelABP-banner-remover.js

--- spiegelABP-banner-remover.js
+++ spiegelABP-banner-remover.js
@@ -8,6 +8,32 @@
 // ==/UserScript==
 
 
+// remove extended 
+setInterval(function(){  
+  // Message
+  var badSpiegel = document.querySelector('div[class*="sp_message_container"]');
+	badSpiegel.remove();
+  // Background
+  var badSpiegel = document.querySelector('div[class*="sp_veil"]');
+	badSpiegel.remove();
+  // Body Scrollbar
+  var badSpiegelBody = document.querySelector('body');
+	if (badSpiegelBody) {
+    badSpiegelBody.setAttribute('style', '!important');
+  }
+  // Body Scrollbar
+  var badSpiegelHtml = document.querySelector('html');
+	if (badSpiegelHtml) {
+    badSpiegelHtml.setAttribute('style', '!important');
+  }
+    
+}, 1000 ); // 2000 means every two seconds (in milliseconds)
+
+// Alternative
+//var badDivs = $("div.SpecificParentName div.SpecificChildName:contains('SpecificTextName')");
+//badDivs.parent().remove();
+
+
 // delete hoveritem
 // note: 'ua-default' is renamed to 'ua-detected ua-ff' later on
 var adWindow = document.getElementsByClassName('ua-default');  
@@ -15,7 +41,7 @@ if (adWindow) {
     adWindow[0].parentNode.removeChild(adWindow[0]);
 }
 
-// remove blurring 
+// remove blurring
 // note: normally not needed
 var aBlurringDiv = document.getElementById('wrapper-content');
 if (aBlurringDiv) {
